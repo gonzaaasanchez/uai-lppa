74a37dd96166b29bf7964d01b2a69bc9c240e365
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2023-06-22 21:47:50 -0300

    ADD submit function

--- class09/validations.js
+++ class09/validations.js
@@ -39,6 +39,7 @@ function validateName() {
         error = "El nombre debe tener, al menos, un espacio.";
     }
     nameError.innerText = error;
+    return error;
 }
 
 function validateEmail() {
@@ -51,6 +52,7 @@ function validateEmail() {
         error = "Formato de email no válido";
     }
     emailError.innerText = error;
+    return error;
 }
 
 function validatePassword() {
@@ -63,6 +65,7 @@ function validatePassword() {
         error = "Debe tener al menos 8 caracteres, formados por letras y números.";
     }
     passwordError.innerText = error;
+    return error;
 }
 
 function validatePasswordRepeat() {
@@ -75,6 +78,7 @@ function validatePasswordRepeat() {
         error = "Las contraseñas no coinciden";
     }
     passwordRepeatError.innerText = error;
+    return error;
 }
 
 function validateAge() {
@@ -86,6 +90,7 @@ function validateAge() {
         error = "La edad debe ser mayor a 18";
     }
     ageError.innerText = error;
+    return error;
 }
 
 function validatePhone() {
@@ -98,6 +103,7 @@ function validatePhone() {
         error = "Debe ser número de al menos 7 dígitos, sin espacios, guiones ni paréntesis.";
     }
     phoneError.innerText = error;
+    return error;
 }
 
 function validateAddress() {
@@ -110,6 +116,7 @@ function validateAddress() {
         error = "Debe tener al menos 5 caracteres, con letras, números y un espacio en el medio.";
     }
     addressError.innerText = error;
+    return error;
 }
 
 function validateCity() {
@@ -121,6 +128,7 @@ function validateCity() {
         error = "Debe tener al menos 3 caracteres.";
     }
     cityError.innerText = error;
+    return error;
 }
 
 function validatePostcode() {
@@ -132,6 +140,7 @@ function validatePostcode() {
         error = "Debe tener al menos 3 caracteres.";
     }
     postcodeError.innerText = error;
+    return error;
 }
 
 function validateID() {
@@ -143,4 +152,56 @@ function validateID() {
         error = "Debe ser número y tener 7 u 8 caracteres";
     }
     idError.innerText = error;
+    return error;
+}
+
+function submitForm() {
+    const errors = [];
+    const success = [];
+
+    var errorName = validateName();
+    if (errorName) errors.push('Campo nombre: ' + errorName);
+    else success.push('Campo nombre: ' + nameInput.value);
+
+    var errorEmail = validateEmail();
+    if (errorEmail) errors.push('Campo email: ' + errorEmail);
+    else success.push('Campo email: ' + emailInput.value);
+
+    var errorPass = validatePassword();
+    if (errorPass) errors.push('Campo contraseña: ' + errorPass);
+    else success.push('Campo contraseña: ' + passwordInput.value);
+
+    var errorPassRepeat = validatePasswordRepeat();
+    if (errorPassRepeat) errors.push('Campo repetir contraseña: ' + errorPassRepeat);
+
+    var errorAge = validateAge();
+    if (errorAge) errors.push('Campo edad: ' + errorAge);
+    else success.push('Campo edad: ' + ageInput.value);
+
+    var errorPhone = validatePhone();
+    if (errorPhone) errors.push('Campo teléfono: ' + errorPhone);
+    else success.push('Campo teléfono: ' + phoneInput.value);
+
+    var errorAddress = validateAddress();
+    if (errorAddress) errors.push('Campo dirección: ' + errorAddress);
+    else success.push('Campo dirección: ' + addressInput.value);
+
+    var errorCity = validateCity();
+    if (errorCity) errors.push('Campo ciudad: ' + errorCity);
+    else success.push('Campo ciudad: ' + cityInput.value);
+
+    var errorCP = validatePostcode();
+    if (errorCP) errors.push('Campo CP: ' + errorCP);
+    else success.push('Campo CP: ' + postcodeInput.value);
+
+    var errorDNI = validateID();
+    if (errorDNI) errors.push('Campo DNI: ' + errorDNI);
+    else success.push('Campo DNI: ' + idInput.value);
+
+    if (errors.length) {
+        alert('El formulario tiene los siguientes errores:\n\n' + errors.join('\n'));
+    } else {
+        console.log('Successss');
+        alert('El formulario fue completado con los siguientes valores:\n\n' + success.join('\n'));
+    }
 }
